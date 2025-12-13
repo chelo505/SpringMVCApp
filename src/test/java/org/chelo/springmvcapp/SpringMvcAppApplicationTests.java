3969e1cb3550c208aad1d302e9531a5f9f51cde1
chelo505 <datobaqro@gmail.com>
2025-12-13 17:52:27 +0400

    init

---
+++ src/test/java/org/chelo/springmvcapp/SpringMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chelo.springmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
